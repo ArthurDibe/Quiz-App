e69e8170aba4ca81f00d2e28ec5b164074e4d77b
Arthur Dibe <dibearthur@gmail.com>
2023-10-19 15:25:32 -0400

    Implemented AnswerButton class for list of answer options

---
+++ lib/models/answer_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+  });
+
+  final String text;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: this.onTap,
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 40,
+            vertical: 10,
+          ),
+          backgroundColor: const Color.fromARGB(255, 82, 0, 154),
+          foregroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+      child: Text(this.text),
+    );
+  }
+}
